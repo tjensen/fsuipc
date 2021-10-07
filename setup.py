262ca6ccdea6285ae8a40720fd021299940ded7a
Tim Jensen <tim.l.jensen@gmail.com>
2021-10-07 18:47:57 -0500

    Removing Python 3.5 support - resolves #7

--- setup.py
+++ setup.py
@@ -48,13 +48,14 @@ setuptools.setup(
         "Environment :: Win32 (MS Windows)",
         "License :: OSI Approved :: MIT License",
         "Operating System :: Microsoft :: Windows",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Games/Entertainment :: Simulation"
     ],
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     setup_requires=["wheel"],
     ext_modules=[setuptools.Extension(
         name="pyuipc",
