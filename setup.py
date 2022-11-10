47b8b77010e374ff645cc1d3204f776632f5a741
Tim Jensen <tim.l.jensen@gmail.com>
2022-11-10 08:54:12 -0600

    Bumping version for 1.5.0 release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ else:
 
 setuptools.setup(
     name="fsuipc",
-    version="1.4.0",
+    version="1.5.0",
     author="Tim Jensen",
     author_email="tim.l.jensen@gmail.com",
     description="Client wrapper for FSUIPC",
